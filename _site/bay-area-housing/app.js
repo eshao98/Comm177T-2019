4f6747dc99c8e11179116010390ad51f67b1fa60
eshao98 <eshao98@stanford.edu>
2019-05-28 20:41:15 -0700

    update with small area free market rent data (2011-2019).

--- _site/bay-area-housing/app.js
+++ _site/bay-area-housing/app.js
@@ -1,5 +1,9 @@
-var width = 700;
-var height = 580;
+// map viewport dimensions
+var width = 800,
+	height = 600,
+	scale = 270000, 
+	latitude = 37.7750,
+	longitude = -122.4183;
 
 // creating svg
 var svg = d3.select("body")
@@ -7,6 +11,27 @@ var svg = d3.select("body")
 	.attr("width", width)
 	.attr("height", height);
 
-// append empty placeholder g element to svg
-// g will contain geometry elements
-var g = svg.append("g")
+// set up map projection, position map
+var projection = d3.geoAlbers()
+	.center([0, latitude]) // sets single standard parallel at latitude
+	.rotate([-longitude, 0]) // sets longitude of origin
+	.parallels([24.6, 43.6])
+	.scale(scale)
+	.translate([width / 2, height / 2]);
+
+var path = d3.geoPath()
+	.projection(projection);
+
+/* ZOOM
+var g = svg.apppend("g")
+	.call(d3.zoom()
+	.scaleExtent([1, 10])
+	.on("zoom", zoomHandler));
+*/
+
+d3_queue.queue()
+	.defer(d3.json, "data/bay-area-topo.json")
+	.defer(d3.csv, "data/test-data.csv")
+	.await(function(error, map_json, data_csv) {
+		console.log(map_json)
+	});
