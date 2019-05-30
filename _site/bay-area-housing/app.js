e2f7162a03f347f797ece2a247dc85ba06b68f14
eshao98 <eshao98@stanford.edu>
2019-05-30 01:59:30 -0700

    join csv data to svg, create map from geojson

--- _site/bay-area-housing/app.js
+++ _site/bay-area-housing/app.js
@@ -1,37 +1,49 @@
 // map viewport dimensions
 var width = 800,
 	height = 600,
-	scale = 270000, 
+	scale = 80000, 
+	// centered on SF (approximately)
 	latitude = 37.7750,
 	longitude = -122.4183;
 
-// creating svg
-var svg = d3.select("body")
-	.append("svg")
-	.attr("width", width)
-	.attr("height", height);
-
 // set up map projection, position map
-var projection = d3.geoAlbers()
-	.center([0, latitude]) // sets single standard parallel at latitude
-	.rotate([-longitude, 0]) // sets longitude of origin
-	.parallels([24.6, 43.6])
+var projection = d3.geoMercator()
+	.center([longitude, latitude])
+	//.rotate([-longitude, 0]) 
+	//.parallels([24.6, 43.6])
 	.scale(scale)
 	.translate([width / 2, height / 2]);
 
 var path = d3.geoPath()
 	.projection(projection);
 
-/* ZOOM
-var g = svg.apppend("g")
+// tooltip
+tooltip = d3.select("body").append("div")
+	.attr("class", "tooltip")
+	.style("opacity", 0);
+
+// creating svg
+var svg = d3.select("#map")
+	.append("svg")
+	.attr("width", "600px")
+	.attr("height", height);
+
+var g = svg.append("g")
 	.call(d3.zoom()
 	.scaleExtent([1, 10])
 	.on("zoom", zoomHandler));
-*/
 
-d3_queue.queue()
-	.defer(d3.json, "data/bay-area-topo.json")
-	.defer(d3.csv, "data/test-data.csv")
-	.await(function(error, map_json, data_csv) {
-		console.log(map_json)
-	});
+d3.json("data/bay-area-zips.geojson").then(function(geojson) {
+	var zips = g.selectAll("path")
+		.data(geojson.features)
+		.enter().append("path")
+		.attr("d", path);
+})
+
+// zoom function from D3 Cookbook
+function zoomHandler() { 
+    var transform = d3.event.transform; 
+    g.attr("transform", "translate(" 
+            + transform.x + "," + transform.y 
+            + ")scale(" + transform.k + ")"); 
+} 
