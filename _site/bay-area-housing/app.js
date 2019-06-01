4db779fb8fc243f00ddee92b34f958c5306d1b00
eshao98 <eshao98@stanford.edu>
2019-06-01 00:05:24 -0700

    add gradient legend for chloropleth map

--- _site/bay-area-housing/app.js
+++ _site/bay-area-housing/app.js
@@ -7,7 +7,9 @@ var width = 800,
 	longitude = -122.4183
 	lowColor = "#edf8b1"
 	highColor = "#c51b8a"
-	rate = "twohw"; // rate you want to test
+	rate = "twohw" // rate you want to test
+	minVal = d3.format("$,")(0)
+	maxVal = d3.format("$,")(100);
 
 // set up map projection, position map
 var projection = d3.geoMercator()
@@ -35,6 +37,54 @@ tooltip = d3.select("body")
 var color = d3.scaleLinear()
 	.domain([10, 100])
 	.range([lowColor, highColor]);
+
+// legend
+var w = 130, h = 200;
+
+var key = d3.select("#map")
+	.append("svg")
+	.attr("width", w)
+	.attr("height", h)
+	.attr("class", "legend");
+
+var legend = key.append("defs")
+	.append("svg:linearGradient")
+	.attr("id", "gradient")
+	.attr("x1", "100%")
+	.attr("y1", "0%")
+	.attr("x2", "100%")
+	.attr("y2", "100%")
+	.attr("spreadMethod", "pad");
+
+legend.append("stop")
+	.attr("offset", "0%")
+	.attr("stop-color", highColor)
+	.attr("stop-opacity", 1);
+
+legend.append("stop")
+	.attr("offset", "100%")
+	.attr("stop-color", lowColor)
+	.attr("stop-opacity", 1);
+
+key.append("rect")
+	.attr("width", w-100)
+	.attr("height", h)
+	.style("fill", "url(#gradient)")
+	.attr("transform", "translate(30,10)");
+
+var y = d3.scaleLinear()
+	.range([h, 0])
+	.domain([0, 100]);
+
+var yAxis = d3.axisLeft(y)
+	.scale(y)
+	.ticks(5);
+
+key.append("g")
+	.attr("class", "y axis")
+	.attr("transform", "translate(25,10)")
+	.call(yAxis);
+
 
 // creates basic map from geojson
 d3.json("data/bay-area-zips.geojson").then(function(geojson) {
@@ -90,14 +140,19 @@ d3.json("data/bay-area-zips.geojson").then(function(geojson) {
 			return color(filteredData[rate])
 		})
 
-		.on("mouseover", function(d) {
+		.on("mouseover", function(filteredData) {
 			tooltip.transition()
 			.duration(200)
-			.style("opacity", 1)
-			.text("test")
+			.style("opacity", 1);
+			tooltip.html(
+				"<b>" + filteredData.zip + "</b><br>"
+				+ "<p><b>" + filteredData.city + ", " + filteredData.county + "</b><br>" 
+				+ filteredData.year + " Housing Wage: $" + filteredData[rate]
+				)
 			.style("left", (d3.event.pageX - 20) + "px")
 			.style("top", (d3.event.pageY + 20) + "px");
 		})
+
 		.on("mouseout", function(d) {
 			tooltip.transition()
 			.duration(200)
@@ -110,6 +165,8 @@ d3.json("data/bay-area-zips.geojson").then(function(geojson) {
 	d3.select("#menu")
 	.on("input", function() {
 		drawMap();
+
+
 	});
 
 });
